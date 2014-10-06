52169eabb90118e9a1e8578ca5d285cee380d698
cszabo503 <ce_szabo@swbell.net>
2014-10-05 22:01:45 -0500

    added new array and loop exercises, show friends

---
+++ js-functions/arrays-loops/array-exercise.js
@@ -0,0 +1,17 @@
+
+// Add the names of everyone at your table   
+var friends = ['Alex', 'Elyse', 'Mae', 'Sartaj'];
+
+var list = "<ul>"
+
+// Create your for loop here
+// HINT: the string should look like '<li>' + friends[x] + "</li>" + ....
+
+for() {
+
+}
+
+list = list + '</li>'
+
+// Use jQuery to select the body tag on the index and append the finish table string
+$("body").append(list);
